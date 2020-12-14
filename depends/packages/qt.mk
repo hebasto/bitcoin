e6a2c5817b51bec9bad64961143297c22907e643
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-12-14 02:46:28 +0200

    depends: Designate targets in qt.pro

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -260,27 +260,15 @@ define $(package)_config_cmds
   export PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig  && \
   ./configure -top-level $($(package)_config_opts) && \
   echo "host_build: QT_CONFIG ~= s/system-zlib/zlib" >> mkspecs/qconfig.pri && \
-  echo "CONFIG += force_bootstrap" >> mkspecs/qconfig.pri && \
-  bin/qmake -o Makefile qtbase.pro && \
-  $(MAKE) sub-src-clean && \
-  cd ../qttranslations && ../qtbase/bin/qmake qttranslations.pro -o Makefile && \
-  cd translations && ../../qtbase/bin/qmake translations.pro -o Makefile && cd ../.. && \
-  cd qttools/src/linguist/lrelease/ && ../../../../qtbase/bin/qmake lrelease.pro -o Makefile && \
-  cd ../lupdate/ && ../../../../qtbase/bin/qmake lupdate.pro -o Makefile && cd ../../../..
+  echo "CONFIG += force_bootstrap" >> mkspecs/qconfig.pri
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C src $(addprefix sub-,$($(package)_qt_libs)) && \
-  $(MAKE) -C ../qttools/src/linguist/lrelease && \
-  $(MAKE) -C ../qttools/src/linguist/lupdate && \
-  $(MAKE) -C ../qttranslations
+  cd .. && $(MAKE)
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) -C src INSTALL_ROOT=$($(package)_staging_dir) $(addsuffix -install_subtargets,$(addprefix sub-,$($(package)_qt_libs))) && cd .. && \
-  $(MAKE) -C qttools/src/linguist/lrelease INSTALL_ROOT=$($(package)_staging_dir) install_target && \
-  $(MAKE) -C qttools/src/linguist/lupdate INSTALL_ROOT=$($(package)_staging_dir) install_target && \
-  $(MAKE) -C qttranslations INSTALL_ROOT=$($(package)_staging_dir) install_subtargets && \
+  cd .. && $(MAKE) install INSTALL_ROOT=$($(package)_staging_dir) && \
   if `test -f qtbase/src/plugins/platforms/xcb/xcb-static/libxcb-static.a`; then \
     cp qtbase/src/plugins/platforms/xcb/xcb-static/libxcb-static.a $($(package)_staging_prefix_dir)/lib; \
   fi
--- depends/patches/qt/qt.pro
+++ depends/patches/qt/qt.pro
@@ -2,16 +2,41 @@
 cache(, super)
 
 TEMPLATE = subdirs
-SUBDIRS = qtbase qttools qttranslations
+SUBDIRS = qtbase
 
-qtbase.target = module-qtbase
+SUBDIRS += bitcoincore_corelib
+bitcoincore_corelib.subdir = qtbase/src/corelib
+bitcoincore_corelib.depends = qtbase
 
-qttools.target = module-qttools
-qttools.depends = qtbase
+SUBDIRS += bitcoincore_gui
+bitcoincore_gui.subdir = qtbase/src/gui
+bitcoincore_gui.depends = bitcoincore_corelib
 
-qttranslations.target = module-qttranslations
-qttranslations.depends = qttools
+SUBDIRS += bitcoincore_widgets
+bitcoincore_widgets.subdir = qtbase/src/widgets
+bitcoincore_widgets.depends = bitcoincore_corelib bitcoincore_gui
 
-QT_SKIP_MODULES =
+SUBDIRS += bitcoincore_network
+bitcoincore_network.subdir = qtbase/src/network
+bitcoincore_network.depends = bitcoincore_corelib
+
+SUBDIRS += bitcoincore_plugins
+bitcoincore_plugins.subdir = qtbase/src/plugins
+bitcoincore_plugins.depends = bitcoincore_corelib bitcoincore_gui
+
+SUBDIRS += bitcoincore_testlib
+bitcoincore_testlib.subdir = qtbase/src/testlib
+bitcoincore_testlib.depends = bitcoincore_corelib
+
+SUBDIRS += bitcoincore_lrelease
+bitcoincore_lrelease.subdir = qttools/src/linguist/lrelease
+bitcoincore_lrelease.depends = bitcoincore_widgets
+
+SUBDIRS += bitcoincore_lupdate
+bitcoincore_lupdate.subdir = qttools/src/linguist/lupdate
+bitcoincore_lupdate.depends = bitcoincore_widgets
+
+SUBDIRS += qttranslations
+qttranslations.depends = bitcoincore_lrelease bitcoincore_lupdate bitcoincore_widgets
 
 load(qt_configure)
