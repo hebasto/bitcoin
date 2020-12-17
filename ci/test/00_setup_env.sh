2294f45bf97efd87deea7792baf753538ebe0a64
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-12-17 20:28:17 +0200

    export HEBASTO="hebasto_42

--- ci/test/00_setup_env.sh
+++ ci/test/00_setup_env.sh
@@ -70,3 +70,5 @@ export GOAL=${GOAL:-install}
 export DIR_QA_ASSETS=${DIR_QA_ASSETS:-${BASE_SCRATCH_DIR}/qa-assets}
 export PATH=${BASE_ROOT_DIR}/ci/retry:$PATH
 export CI_RETRY_EXE=${CI_RETRY_EXE:-"retry --"}
+
+export HEBASTO="hebasto_42"
