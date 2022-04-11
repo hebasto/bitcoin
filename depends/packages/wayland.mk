127dbe316fb4a15869f976e388ca4d8d6f45ed06
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-04-11 23:59:12 +0200

    Wayland 1.20 -- WIP

--- depends/packages/wayland.mk
+++ depends/packages/wayland.mk
@@ -1,22 +1,16 @@
 package := wayland
-$(package)_version := 1.19.0
+$(package)_version := 1.20.0
 $(package)_download_path := https://wayland.freedesktop.org/releases
 $(package)_file_name := wayland-$($(package)_version).tar.xz
-$(package)_sha256_hash := baccd902300d354581cd5ad3cc49daa4921d55fb416a5883e218750fef166d15
+$(package)_sha256_hash := b8a034154c7059772e0fdbd27dbfcda6c732df29cae56a82274f6ec5d7cd8725
 $(package)_dependencies := libffi expat
 
-define $(package)_set_vars
-  $(package)_config_opts := --enable-option-checking --disable-dependency-tracking
-  $(package)_config_opts += --enable-shared --disable-static --disable-documentation
-  $(package)_config_opts += --disable-dtd-validation --with-host-scanner
-endef
-
 define $(package)_config_cmds
-  PATH="/usr/bin:$(PATH)" $($(package)_autoconf)
+  PKG_CONFIG_LIBDIR=/usr/lib/x86_64-linux-gnu/pkgconfig meson -Dscanner=false -Dtests=false -Ddocumentation=false -Ddtd_validation=false build/ --prefix=$($(package)_staging_dir)
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  ninja -C build/
 endef
 
 define $(package)_stage_cmds
