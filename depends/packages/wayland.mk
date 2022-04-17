5b8add1d808db6224c2ef9a6d235fce25beb745e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-04-17 15:48:54 +0200

    built archive is OK

--- depends/packages/wayland.mk
+++ depends/packages/wayland.mk
@@ -6,11 +6,7 @@ $(package)_sha256_hash := b8a034154c7059772e0fdbd27dbfcda6c732df29cae56a82274f6e
 
 define $(package)_config_cmds
   export PKG_CONFIG_WAYLAND_SCANNER_WAYLAND_SCANNER=$$$$(env -u PKG_CONFIG_LIBDIR PKG_CONFIG_PATH=$(SYSTEM_PKG_CONFIG_PATH) pkg-config --variable=wayland_scanner wayland-scanner) && \
-  meson -Dscanner=false -Dtests=false -Ddocumentation=false -Ddtd_validation=false build/ --prefix=/
-endef
-
-define $(package)_build_cmds
-  ninja -C build/
+  meson -Dscanner=false -Dtests=false -Ddocumentation=false -Ddtd_validation=false build/ --prefix=$($($(package)_type)_prefix)
 endef
 
 define $(package)_stage_cmds
