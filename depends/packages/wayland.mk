83e16051250d474a0009c23bc47771d15ead5fcc
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-04-17 15:26:34 +0200

    wip.3

--- depends/packages/wayland.mk
+++ depends/packages/wayland.mk
@@ -6,7 +6,7 @@ $(package)_sha256_hash := b8a034154c7059772e0fdbd27dbfcda6c732df29cae56a82274f6e
 
 define $(package)_config_cmds
   export PKG_CONFIG_WAYLAND_SCANNER_WAYLAND_SCANNER=$$$$(env -u PKG_CONFIG_LIBDIR PKG_CONFIG_PATH=$(SYSTEM_PKG_CONFIG_PATH) pkg-config --variable=wayland_scanner wayland-scanner) && \
-  meson -Dscanner=false -Dtests=false -Ddocumentation=false -Ddtd_validation=false build/ --prefix=$($(package)_staging_dir)
+  meson -Dscanner=false -Dtests=false -Ddocumentation=false -Ddtd_validation=false build/ --prefix=/
 endef
 
 define $(package)_build_cmds
@@ -14,9 +14,5 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
-
-define $(package)_postprocess_cmds
-  rm lib/*.la
+  DESTDIR=$($(package)_staging_dir) ninja -C build/ install
 endef
