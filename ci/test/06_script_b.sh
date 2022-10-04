7f0a85949a8e6ce3328129882068af4a86258854
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-10-04 21:15:04 +0100

    ci: Lint and Tidy tasks only

--- ci/test/06_script_b.sh
+++ ci/test/06_script_b.sh
@@ -36,8 +36,6 @@ fi
 
 if [ "${RUN_TIDY}" = "true" ]; then
   set -eo pipefail
-  export P_CI_DIR="${BASE_BUILD_DIR}/bitcoin-$HOST/src/"
-  ( CI_EXEC run-clang-tidy -quiet "${MAKEJOBS}" ) | grep -C5 "error"
   export P_CI_DIR="${BASE_BUILD_DIR}/bitcoin-$HOST/"
   CI_EXEC "python3 ${DIR_IWYU}/include-what-you-use/iwyu_tool.py"\
           " src/common/url.cpp"\
