962c1e0b24f1b0279204511dd2d072859aa1b548
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-11-17 11:22:12 +0000

    Use Intel's docs

--- src/bench/bench_bitcoin.cpp
+++ src/bench/bench_bitcoin.cpp
@@ -59,8 +59,31 @@ static uint8_t parsePriorityLevel(const std::string& str) {
     return levels;
 }
 
+
+int CheckForIntelShaExtensions() {
+   int a, b, c, d;
+
+   // Look for CPUID.7.0.EBX[29]
+   // EAX = 7, ECX = 0
+   a = 7;
+   c = 0;
+
+   __asm__ volatile ("cpuid"
+        :"=a"(a), "=b"(b), "=c"(c), "=d"(d)
+        :"a"(a), "c"(c)
+       );
+
+   // Intel® SHA Extensions feature bit is EBX[29]
+   return ((b >> 29) & 1);
+}
+
+
 int main(int argc, char** argv)
 {
+#ifdef __SHA__
+    #error
+#endif
+    std::cout << CheckForIntelShaExtensions() << std::endl;
     std::cout << SHA256AutoDetect() << std::endl;
     ArgsManager argsman;
     SetupBenchArgs(argsman);
