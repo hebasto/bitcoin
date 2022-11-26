97450cef5126d2dad3a0cbdc337b5c50debaf9a4
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-11-26 10:43:19 +0000

    ci: Use pyenv in lint task

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -7,7 +7,19 @@
 export LC_ALL=C
 
 ${CI_RETRY_EXE} apt-get update
-${CI_RETRY_EXE} apt-get install -y python3-pip curl git gawk jq
+${CI_RETRY_EXE} apt-get install -y curl git gawk jq
+
+export PYENV_ROOT="${HOME}/.pyenv"
+PYTHON_VERSION=$(cat "${BASE_ROOT_DIR}/.python-version")
+PYTHON_BIN_PATH="${PYENV_ROOT}/versions/${PYTHON_VERSION}/bin"
+if [ ! -d "$PYTHON_BIN_PATH" ]; then
+  ${CI_RETRY_EXE} apt-get install -y build-essential libbz2-dev libncursesw5-dev libreadline-dev libssl-dev libsqlite3-dev liblzma-dev zlib1g-dev
+  curl -sL https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+  "${PYENV_ROOT}/bin/pyenv" install "$PYTHON_VERSION"
+fi
+export PATH="${PYTHON_BIN_PATH}:${PATH}"
+python --version
+
 (
   # Temporary workaround for https://github.com/bitcoin/bitcoin/pull/26130#issuecomment-1260499544
   # Can be removed once the underlying image is bumped to something that includes git2.34 or later
@@ -16,11 +28,11 @@ ${CI_RETRY_EXE} apt-get install -y python3-pip curl git gawk jq
   ${CI_RETRY_EXE} apt-get install -y --reinstall git
 )
 
-${CI_RETRY_EXE} pip3 install codespell==2.2.1
-${CI_RETRY_EXE} pip3 install flake8==4.0.1
-${CI_RETRY_EXE} pip3 install mypy==0.942
-${CI_RETRY_EXE} pip3 install pyzmq==22.3.0
-${CI_RETRY_EXE} pip3 install vulture==2.3
+${CI_RETRY_EXE} python -m pip install codespell==2.2.1
+${CI_RETRY_EXE} python -m pip install flake8==4.0.1
+${CI_RETRY_EXE} python -m pip install mypy==0.942
+${CI_RETRY_EXE} python -m pip install pyzmq==22.3.0
+${CI_RETRY_EXE} python -m pip install vulture==2.3
 
 SHELLCHECK_VERSION=v0.8.0
 curl -sL "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
