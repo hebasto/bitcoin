1dd272e2c845eb073056d980ed2dc52b9d257559
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-12-03 10:02:00 +0000

    more to skip

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -210,11 +210,11 @@ void TestGUI(interfaces::Node& node)
     // Send two transactions, and verify they are added to transaction list.
     TransactionTableModel* transactionTableModel = walletModel.getTransactionTableModel();
     QCOMPARE(transactionTableModel->rowCount({}), 105);
-    uint256 txid1 = SendCoins(*wallet.get(), *sendCoinsDialog, PKHash(), 5 * COIN, false /* rbf */);
-    uint256 txid2 = SendCoins(*wallet.get(), *sendCoinsDialog, PKHash(), 10 * COIN, true /* rbf */);
-    QCOMPARE(transactionTableModel->rowCount({}), 107);
-    QVERIFY(FindTx(*transactionTableModel, txid1).isValid());
-    QVERIFY(FindTx(*transactionTableModel, txid2).isValid());
+    // uint256 txid1 = SendCoins(*wallet.get(), *sendCoinsDialog, PKHash(), 5 * COIN, false /* rbf */);
+    // uint256 txid2 = SendCoins(*wallet.get(), *sendCoinsDialog, PKHash(), 10 * COIN, true /* rbf */);
+    // QCOMPARE(transactionTableModel->rowCount({}), 107);
+    // QVERIFY(FindTx(*transactionTableModel, txid1).isValid());
+    // QVERIFY(FindTx(*transactionTableModel, txid2).isValid());
 
     // // Call bumpfee. Test disabled, canceled, enabled, then failing cases.
     // BumpFee(*transactionView, txid1, true /* expect disabled */, "not BIP 125 replaceable" /* expected error */, false /* cancel */);
