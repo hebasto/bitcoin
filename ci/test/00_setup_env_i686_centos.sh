2b8830cc2faa7ed3864b7694ab236dbfc620a928
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-01-24 13:13:36 +0000

    fixup

--- ci/test/00_setup_env_i686_centos.sh
+++ ci/test/00_setup_env_i686_centos.sh
@@ -12,6 +12,7 @@ export CI_IMAGE_NAME_TAG=quay.io/centos/centos:stream8
 export CI_BASE_PACKAGES="clang gcc-c++ glibc-devel.x86_64 libstdc++-devel.x86_64 glibc-devel.i686 libstdc++-devel.i686 ccache libtool make git python38 python38-pip which patch lbzip2 xz procps-ng dash rsync coreutils bison"
 export PIP_PACKAGES="pyzmq"
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-zmq --with-gui=qt5 --enable-reduce-exports CC=clang CXX=clang++"
+export DEP_OPTS="CC='clang' CXX='clang++'"
+export BITCOIN_CONFIG="--enable-zmq --with-gui=qt5 --enable-reduce-exports"
 export CONFIG_SHELL="/bin/dash"
 export TEST_RUNNER_ENV="LC_ALL=en_US.UTF-8"
