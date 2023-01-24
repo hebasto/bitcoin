08b36d8a3cf6978f10546c5d487aed6cc5bcc45d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-01-24 14:54:09 +0000

    i686

--- ci/test/00_setup_env_i686_centos.sh
+++ ci/test/00_setup_env_i686_centos.sh
@@ -9,7 +9,7 @@ export LC_ALL=C.UTF-8
 export HOST=i686-pc-linux-gnu
 export CONTAINER_NAME=ci_i686_centos
 export CI_IMAGE_NAME_TAG=quay.io/centos/centos:stream8
-export CI_BASE_PACKAGES="clang-14 gcc-c++ glibc-devel.x86_64 libstdc++-devel.x86_64 glibc-devel.i686 libstdc++-devel.i686 ccache libtool make git python38 python38-pip which patch lbzip2 xz procps-ng dash rsync coreutils bison"
+export CI_BASE_PACKAGES="clang.i686 clang.x86_64 gcc-c++ glibc-devel.x86_64 libstdc++-devel.x86_64 glibc-devel.i686 libstdc++-devel.i686 ccache libtool make git python38 python38-pip which patch lbzip2 xz procps-ng dash rsync coreutils bison"
 export PIP_PACKAGES="pyzmq"
 export GOAL="install"
 export DEP_OPTS="CC='clang' CXX='clang++'"
