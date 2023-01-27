96b683ee9f35c487299021fa6bcb89c0ca429cd8
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-01-27 15:13:12 +0000

    build: Update `libmultiprocess`

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=1af83d15239ccfa7e47b8764029320953dd7fdf1
-$(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
+$(package)_version=d795e969dd7c913c937a4f97d78115072feb6cb4
+$(package)_download_path=https://github.com/hebasto/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=e5587d3feedc7f8473f178a89b94163a11076629825d664964799bbbd5844da5
+$(package)_sha256_hash=280534ef1a4bd82855513ae74afeb251bbdaa1e46b351ae3262c0a5771711248
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
