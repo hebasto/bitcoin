5c5a376d9c0ab5fac1af86696fca6834402f2158
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-04-19 12:17:07 +0100

    fixup

--- ci/test/00_setup_env_native_tsan.sh
+++ ci/test/00_setup_env_native_tsan.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_tsan
 export DOCKER_NAME_TAG=ubuntu:22.04
-export PACKAGES="clang-12 llvm-12 libc++abi-12-dev libc++-12-dev python3-zmq"
+export PACKAGES="clang-12 llvm-12 libc++abi-12-dev libc++-12-dev python3-zmq libunwind-dev"
 export DEP_OPTS="NO_QT=1 NO_ZMQ=1 CC=clang-12 CXX='clang++-12 -stdlib=libc++'"
 export GOAL="install"
 export BITCOIN_CONFIG="--disable-fuzz-binary --disable-tests --disable-bench CPPFLAGS='-DARENA_DEBUG -DDEBUG_LOCKORDER' CXXFLAGS='-g' --with-sanitizers=thread"
