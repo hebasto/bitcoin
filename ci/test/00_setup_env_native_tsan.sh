954711760f5c0fff47d9d34863c4a2d75735443a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-04-19 10:05:21 +0100

    Switch to clang-12

--- ci/test/00_setup_env_native_tsan.sh
+++ ci/test/00_setup_env_native_tsan.sh
@@ -8,8 +8,8 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_tsan
 export DOCKER_NAME_TAG=ubuntu:20.04
-export PACKAGES="clang llvm libc++abi-dev libc++-dev python3-zmq"
-export DEP_OPTS="NO_QT=1 CC=clang CXX='clang++ -stdlib=libc++'"
+export PACKAGES="clang-12 llvm-12 libc++abi-12-dev libc++-12-dev python3-zmq"
+export DEP_OPTS="NO_QT=1 CC=clang-12 CXX='clang++-12 -stdlib=libc++'"
 export GOAL="install"
-export BITCOIN_CONFIG="--disable-fuzz-binary --disable-tests --disable-bench --enable-zmq --with-gui=no CPPFLAGS='-DARENA_DEBUG -DDEBUG_LOCKORDER' CXXFLAGS='-g' --with-sanitizers=thread CC=clang CXX='clang++ -stdlib=libc++' --with-boost-process"
+export BITCOIN_CONFIG="--disable-fuzz-binary --disable-tests --disable-bench --enable-zmq --with-gui=no CPPFLAGS='-DARENA_DEBUG -DDEBUG_LOCKORDER' CXXFLAGS='-g' --with-sanitizers=thread --with-boost-process"
 export RUN_UNIT_TESTS=false
