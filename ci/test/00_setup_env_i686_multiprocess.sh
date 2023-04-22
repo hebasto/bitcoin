164854f769f7b83ac7c3b492da3c56d72e66b7eb
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-04-22 12:28:00 +0100

    Fixup

--- ci/test/00_setup_env_i686_multiprocess.sh
+++ ci/test/00_setup_env_i686_multiprocess.sh
@@ -6,10 +6,10 @@
 
 export LC_ALL=C.UTF-8
 
-export HOST=i686-pc-linux-gnu
+export HOST=i686-linux-gnu
 export CONTAINER_NAME=ci_i686_multiprocess
 export CI_IMAGE_NAME_TAG=ubuntu:20.04
-export PACKAGES="cmake python3 llvm clang g++-multilib"
+export PACKAGES="cmake python3 llvm clang g++-i686-linux-gnu binutils-i686-linux-gnu"
 export DEP_OPTS="DEBUG=1 MULTIPROCESS=1"
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-debug CC='clang -m32' CXX='clang++ -m32' LDFLAGS='--rtlib=compiler-rt -lgcc_s'"
