854ec32e6f65d295fc6a3205055fd9ddb7a3264e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-05-19 14:58:53 +0100

    Use `setup.py`

---
+++ test/functional/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017-2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bitcoin_functional_tests',
+    version='0.1',
+    packages=find_packages(),
+)
