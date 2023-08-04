6b6ddc719e201f45e21dc69a445d5d5623e591ce
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-08-04 11:08:07 +0100

    Speed up run

--- ci/test/00_setup_env_mac_native.sh
+++ ci/test/00_setup_env_mac_native.sh
@@ -9,10 +9,11 @@ export LC_ALL=C.UTF-8
 export HOST=x86_64-apple-darwin
 export PIP_PACKAGES="zmq"
 export GOAL="install"
-export BITCOIN_CONFIG="--with-gui --with-miniupnpc --with-natpmp --enable-reduce-exports"
+export BITCOIN_CONFIG="--without-gui --with-miniupnpc --with-natpmp --enable-reduce-exports"
 export CI_OS_NAME="macos"
 export NO_DEPENDS=1
 export OSX_SDK=""
 export CCACHE_SIZE=400M
-export RUN_FUZZ_TESTS=true
-export FUZZ_TESTS_CONFIG="--exclude banman"  # https://github.com/bitcoin/bitcoin/issues/27924
+
+export RUN_UNIT_TESTS=false
+export RUN_FUNCTIONAL_TESTS=false
