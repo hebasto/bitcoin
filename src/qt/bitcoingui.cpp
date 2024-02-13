bitcoin-core/gui

qt: Post user notifications using QSystemTrayIcon on macOS

src/qt/bitcoingui.cpp
@@ -35,6 +35,7 @@
 #include <interfaces/handler.h>
 #include <interfaces/node.h>
 #include <node/ui_interface.h>
+#include <util/check.h>
 #include <util/system.h>
 #include <util/translation.h>
 #include <validation.h>
@@ -63,6 +64,7 @@
 #include <QVBoxLayout>
 #include <QWindow>
 
+#include <cassert>
 
 const std::string BitcoinGUI::DEFAULT_UIPLATFORM =
 #if defined(Q_OS_MAC)
@@ -610,9 +612,13 @@ void BitcoinGUI::setClientModel(ClientModel *_clientModel, interfaces::BlockAndH
             walletFrame->setClientModel(_clientModel);
         }
 #endif // ENABLE_WALLET
-        unitDisplayControl->setOptionsModel(_clientModel->getOptionsModel());
 
         OptionsModel* optionsModel = _clientModel->getOptionsModel();
+        unitDisplayControl->setOptionsModel(optionsModel);
+#ifdef Q_OS_MAC
+        // trayIcon is used to post user notifications to macOS Notification Center.
+        Assert(trayIcon)->show();
+#else
         if (optionsModel && trayIcon) {
             // be aware of the tray icon disable state change reported by the OptionsModel object.
             connect(optionsModel, &OptionsModel::hideTrayIconChanged, this, &BitcoinGUI::setTrayIconVisible);
@@ -620,6 +626,7 @@ void BitcoinGUI::setClientModel(ClientModel *_clientModel, interfaces::BlockAndH
             // initialize the disable state of the tray icon with the current value in the model.
             setTrayIconVisible(optionsModel->getHideTrayIcon());
         }
+#endif // Q_OS_MAC
     } else {
         // Disable possibility to show main window via action
         toggleHideAction->setEnabled(false);
@@ -752,14 +759,8 @@ void BitcoinGUI::setWalletActionsEnabled(bool enabled)
 void BitcoinGUI::createTrayIcon()
 {
     assert(QSystemTrayIcon::isSystemTrayAvailable());
-
-#ifndef Q_OS_MAC
-    if (QSystemTrayIcon::isSystemTrayAvailable()) {
-        trayIcon = new QSystemTrayIcon(m_network_style->getTrayAndWindowIcon(), this);
-        QString toolTip = tr("%1 client").arg(PACKAGE_NAME) + " " + m_network_style->getTitleAddText();
-        trayIcon->setToolTip(toolTip);
-    }
-#endif
+    trayIcon = new QSystemTrayIcon(m_network_style->getTrayAndWindowIcon(), this);
+    trayIcon->setToolTip(tr("%1 client").arg(PACKAGE_NAME) + " " + m_network_style->getTitleAddText());
 }
 
 void BitcoinGUI::createTrayIconMenu()

hebasto/bitcoin

src/qt/bitcoingui.cpp
@@ -36,6 +36,7 @@
 #include <interfaces/handler.h>
 #include <interfaces/node.h>
 #include <node/interface_ui.h>
+#include <util/check.h>
 #include <util/translation.h>
 #include <validation.h>
 
@@ -69,6 +70,7 @@
 #include <QVBoxLayout>
 #include <QWindow>
 
+#include <cassert>
 
 const std::string BitcoinGUI::DEFAULT_UIPLATFORM =
 #if defined(Q_OS_MACOS)
@@ -637,9 +639,13 @@ void BitcoinGUI::setClientModel(ClientModel *_clientModel, interfaces::BlockAndH
             walletFrame->setClientModel(_clientModel);
         }
 #endif // ENABLE_WALLET
-        unitDisplayControl->setOptionsModel(_clientModel->getOptionsModel());
 
         OptionsModel* optionsModel = _clientModel->getOptionsModel();
+        unitDisplayControl->setOptionsModel(optionsModel);
+#ifdef Q_OS_MACOS
+        // trayIcon is used to post user notifications to macOS Notification Center.
+        Assert(trayIcon)->show();
+#else
         if (optionsModel && trayIcon) {
             // be aware of the tray icon disable state change reported by the OptionsModel object.
             connect(optionsModel, &OptionsModel::showTrayIconChanged, trayIcon, &QSystemTrayIcon::setVisible);
@@ -647,6 +653,7 @@ void BitcoinGUI::setClientModel(ClientModel *_clientModel, interfaces::BlockAndH
             // initialize the disable state of the tray icon with the current value in the model.
             trayIcon->setVisible(optionsModel->getShowTrayIcon());
         }
+#endif // Q_OS_MACOS
 
         m_mask_values_action->setChecked(_clientModel->getOptionsModel()->getOption(OptionsModel::OptionID::MaskValues).toBool());
     } else {
@@ -810,14 +817,8 @@ void BitcoinGUI::setWalletActionsEnabled(bool enabled)
 void BitcoinGUI::createTrayIcon()
 {
     assert(QSystemTrayIcon::isSystemTrayAvailable());
-
-#ifndef Q_OS_MACOS
-    if (QSystemTrayIcon::isSystemTrayAvailable()) {
-        trayIcon = new QSystemTrayIcon(m_network_style->getTrayAndWindowIcon(), this);
-        QString toolTip = tr("%1 client").arg(PACKAGE_NAME) + " " + m_network_style->getTitleAddText();
-        trayIcon->setToolTip(toolTip);
-    }
-#endif
+    trayIcon = new QSystemTrayIcon(m_network_style->getTrayAndWindowIcon(), this);
+    trayIcon->setToolTip(tr("%1 client").arg(PACKAGE_NAME) + " " + m_network_style->getTitleAddText());
 }
 
 void BitcoinGUI::createTrayIconMenu()