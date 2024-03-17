3be62d0fd0d2db2a1bcb80b6911121e12ed55815
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-03-17 15:27:40 +0000

    ci, gha: Basics and "macOS native" job

--- ci/test/00_setup_env_mac_native.sh
+++ ci/test/00_setup_env_mac_native.sh
@@ -7,9 +7,11 @@
 export LC_ALL=C.UTF-8
 
 export HOST=x86_64-apple-darwin
-export PIP_PACKAGES="zmq"
+# Homebrew's python@3.12 is marked as externally managed (PEP 668).
+# Therefore, `--break-system-packages` is needed.
+export PIP_PACKAGES="--break-system-packages zmq"
 export GOAL="install"
-export BITCOIN_CONFIG="--with-gui --with-miniupnpc --with-natpmp --enable-reduce-exports"
+export BITCOIN_CONFIG="-DWITH_GUI=Qt5 -DWITH_MINIUPNPC=ON -DWITH_NATPMP=ON -DREDUCE_EXPORTS=ON"
 export CI_OS_NAME="macos"
 export NO_DEPENDS=1
 export OSX_SDK=""
