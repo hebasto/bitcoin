3021f3a21fba747ea3cf6fb0b34aec6c74e7020a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-03-29 11:52:28 +0000

    depends: Switch libmultiprocess branch to PR96

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=8da797c5f1644df1bffd84d10c1ae9836dc70d60
-$(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
+$(package)_version=4e70ad47191f85188da869400690c22f6f81e1e0
+$(package)_download_path=https://github.com/hebasto/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=030f4d393d2ac9deba98d2e1973e22fc439ffc009d5f8ae3225c90639f86beb0
+$(package)_sha256_hash=1984697d6937a6e5aeede421fb35e5711ba7063b622ab9e4afe4e0558d1fe5f8
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
