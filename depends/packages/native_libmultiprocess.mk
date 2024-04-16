15a65d480a9a1dc76346c2ebc56a2eafb881d2df
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-04-16 10:33:14 +0100

    build: Introduce CMake-based build system

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=8b8a4766ce0a1892b9e8a5eb73dc39821005e520
+$(package)_version=003eb04d6d0029fd24a330ab63d5a9ba08cf240f
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=475c0dc2357a2ff30e9a164e4c16dc8a6597a57c9193d646fa9cbf0a55c45d78
+$(package)_sha256_hash=d23e82f7a0b498a876a4bcdecca3104032a9f9372e1a0cf0049409a2718e5d39
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
