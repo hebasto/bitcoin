ee5096ac4e638f088c78850a5d2e401fbe88537f
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-04-19 13:57:00 +0100

    Remove `leveldb::port::kLittleEndian`. An alternative implementation

--- src/leveldb/util/coding.h
+++ src/leveldb/util/coding.h
@@ -10,6 +10,7 @@
 #ifndef STORAGE_LEVELDB_UTIL_CODING_H_
 #define STORAGE_LEVELDB_UTIL_CODING_H_
 
+#include <bit>
 #include <cstdint>
 #include <cstring>
 #include <string>
@@ -48,9 +49,8 @@ int VarintLength(uint64_t v);
 char* EncodeVarint32(char* dst, uint32_t value);
 char* EncodeVarint64(char* dst, uint64_t value);
 
-// TODO(costan): Remove port::kLittleEndian and the fast paths based on
-//               std::memcpy when clang learns to optimize the generic code, as
-//               described in https://bugs.llvm.org/show_bug.cgi?id=41761
+// TODO(costan): Remove the fast paths based on
+//               std::memcpy when MSVC learns to optimize the generic code.
 //
 // The platform-independent code in DecodeFixed{32,64}() gets optimized to mov
 // on x86 and ldr on ARM64, by both clang and gcc. However, only gcc optimizes
@@ -62,7 +62,7 @@ char* EncodeVarint64(char* dst, uint64_t value);
 inline void EncodeFixed32(char* dst, uint32_t value) {
   uint8_t* const buffer = reinterpret_cast<uint8_t*>(dst);
 
-  if (port::kLittleEndian) {
+  if constexpr (std::endian::native == std::endian::little) {
     // Fast path for little-endian CPUs. All major compilers optimize this to a
     // single mov (x86_64) / str (ARM) instruction.
     std::memcpy(buffer, &value, sizeof(uint32_t));
@@ -70,7 +70,7 @@ inline void EncodeFixed32(char* dst, uint32_t value) {
   }
 
   // Platform-independent code.
-  // Currently, only gcc optimizes this to a single mov / str instruction.
+  // Clang and gcc optimize this to a single mov / str instruction.
   buffer[0] = static_cast<uint8_t>(value);
   buffer[1] = static_cast<uint8_t>(value >> 8);
   buffer[2] = static_cast<uint8_t>(value >> 16);
@@ -80,7 +80,7 @@ inline void EncodeFixed32(char* dst, uint32_t value) {
 inline void EncodeFixed64(char* dst, uint64_t value) {
   uint8_t* const buffer = reinterpret_cast<uint8_t*>(dst);
 
-  if (port::kLittleEndian) {
+  if constexpr (std::endian::native == std::endian::little) {
     // Fast path for little-endian CPUs. All major compilers optimize this to a
     // single mov (x86_64) / str (ARM) instruction.
     std::memcpy(buffer, &value, sizeof(uint64_t));
@@ -88,7 +88,7 @@ inline void EncodeFixed64(char* dst, uint64_t value) {
   }
 
   // Platform-independent code.
-  // Currently, only gcc optimizes this to a single mov / str instruction.
+  // Clang and gcc optimize this to a single mov / str instruction.
   buffer[0] = static_cast<uint8_t>(value);
   buffer[1] = static_cast<uint8_t>(value >> 8);
   buffer[2] = static_cast<uint8_t>(value >> 16);
@@ -105,7 +105,7 @@ inline void EncodeFixed64(char* dst, uint64_t value) {
 inline uint32_t DecodeFixed32(const char* ptr) {
   const uint8_t* const buffer = reinterpret_cast<const uint8_t*>(ptr);
 
-  if (port::kLittleEndian) {
+  if constexpr (std::endian::native == std::endian::little) {
     // Fast path for little-endian CPUs. All major compilers optimize this to a
     // single mov (x86_64) / ldr (ARM) instruction.
     uint32_t result;
@@ -124,7 +124,7 @@ inline uint32_t DecodeFixed32(const char* ptr) {
 inline uint64_t DecodeFixed64(const char* ptr) {
   const uint8_t* const buffer = reinterpret_cast<const uint8_t*>(ptr);
 
-  if (port::kLittleEndian) {
+  if constexpr (std::endian::native == std::endian::little) {
     // Fast path for little-endian CPUs. All major compilers optimize this to a
     // single mov (x86_64) / ldr (ARM) instruction.
     uint64_t result;
