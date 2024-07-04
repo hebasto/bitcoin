518c504706b58b46e67bc95bbdae60728fc1923d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-07-04 14:58:55 +0100

    [DEBUG] ASan `feature_reindex.py`

--- ci/test/00_setup_env_native_asan.sh
+++ ci/test/00_setup_env_native_asan.sh
@@ -27,3 +27,6 @@ CPPFLAGS='-DARENA_DEBUG -DDEBUG_LOCKORDER' \
 --with-sanitizers=address,float-divide-by-zero,integer,undefined \
 CC='clang-18 -ftrivial-auto-var-init=pattern' CXX='clang++-18 -ftrivial-auto-var-init=pattern'"
 export CCACHE_MAXSIZE=300M
+
+export RUN_UNIT_TESTS=false
+export TEST_RUNNER_EXTRA="--filter feature_reindex.py"
