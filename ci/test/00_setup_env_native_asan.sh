c4a5cab8983877d3f48731bfccde48011b6ad11d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-07-04 16:33:49 +0100

    `-DWITH_ZMQ=OFF -DBUILD_GUI=OFF`

--- ci/test/00_setup_env_native_asan.sh
+++ ci/test/00_setup_env_native_asan.sh
@@ -22,7 +22,7 @@ export CONTAINER_NAME=ci_native_asan
 export PACKAGES="systemtap-sdt-dev clang-18 llvm-18 libclang-rt-18-dev python3-zmq qtbase5-dev qttools5-dev qttools5-dev-tools libevent-dev libboost-dev libdb5.3++-dev libminiupnpc-dev libnatpmp-dev libzmq3-dev libqrencode-dev libsqlite3-dev ${BPFCC_PACKAGE}"
 export NO_DEPENDS=1
 export GOAL="install"
-export BITCOIN_CONFIG="-DWITH_USDT=ON -DWITH_ZMQ=ON -DWARN_INCOMPATIBLE_BDB=OFF -DBUILD_GUI=ON \
+export BITCOIN_CONFIG="-DWITH_USDT=ON -DWITH_ZMQ=OFF -DWARN_INCOMPATIBLE_BDB=OFF -DBUILD_GUI=OFF \
 -DSANITIZERS=address,float-divide-by-zero,integer,undefined \
 -DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++-18 \
 -DCMAKE_C_FLAGS='-ftrivial-auto-var-init=pattern' \
