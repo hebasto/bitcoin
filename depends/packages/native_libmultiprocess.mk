55783a91655050ce495cd7a164d779313a9b5c2a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-07-23 16:18:00 +0100

    depends: Switch `libmultiprocess` to chaincodelabs/libmultiprocess#103

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=8b8a4766ce0a1892b9e8a5eb73dc39821005e520
-$(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
+$(package)_version=c373a94d5b4b3f9c3ee582d15db61d17525bb9bf
+$(package)_download_path=https://github.com/hebasto/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=475c0dc2357a2ff30e9a164e4c16dc8a6597a57c9193d646fa9cbf0a55c45d78
+$(package)_sha256_hash=7588e8e729886de43753088b103b5deaed2e57d39e3936ad4af54b12e08b42a1
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
