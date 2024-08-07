d5b297bc866e7bd516a9393cf1eee7c7ca01f253
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-08-07 15:57:12 +0100

    fixup! cmake: Build `test_bitcoin` executable
    Just set `CMAKE_REQUIRED_DEFINITIONS` is enough.

--- cmake/module/AddBoostIfNeeded.cmake
+++ cmake/module/AddBoostIfNeeded.cmake
@@ -65,7 +65,7 @@ function(add_boost_if_needed)
   if(BUILD_TESTS)
     # Some package managers, such as vcpkg, vendor Boost.Test separately
     # from the rest of the headers, so we have to check for it individually.
-    list(APPEND CMAKE_REQUIRED_DEFINITIONS -DBOOST_TEST_NO_MAIN)
+    set(CMAKE_REQUIRED_DEFINITIONS -DBOOST_TEST_NO_MAIN)
     include(CheckIncludeFileCXX)
     check_include_file_cxx(boost/test/included/unit_test.hpp HAVE_BOOST_INCLUDED_UNIT_TEST_H)
     if(NOT HAVE_BOOST_INCLUDED_UNIT_TEST_H)
