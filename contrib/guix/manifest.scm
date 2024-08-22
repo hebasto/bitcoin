7ebb5017bfdbc831be75ae1038700f44002d89d8
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-08-22 16:12:15 +0100

    guix: Bump `lief` up to 0.15.1

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -169,7 +169,7 @@ chain for " target " development."))
 (define-public python-lief
   (package
     (name "python-lief")
-    (version "0.13.2")
+    (version "0.15.1")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -187,19 +187,9 @@ chain for " target " development."))
                      (string-append m (number->string (parallel-job-count)))))))
               (sha256
                (base32
-                "0y48x358ppig5xp97ahcphfipx7cg9chldj2q5zrmn610fmi4zll"))))
-    (build-system python-build-system)
+                "00p6apkzawbqava4cn0ha82mn21d4zqchg2vd66s8pnqkvmfjvyr"))))
+    (build-system cmake-build-system)
     (native-inputs (list cmake-minimal python-tomli))
-    (arguments
-     (list
-      #:tests? #f                  ;needs network
-      #:phases #~(modify-phases %standard-phases
-                   (add-before 'build 'change-directory
-                     (lambda _
-                       (chdir "api/python")))
-                   (replace 'build
-                     (lambda _
-                       (invoke "python" "setup.py" "build"))))))
     (home-page "https://github.com/lief-project/LIEF")
     (synopsis "Library to instrument executable formats")
     (description
