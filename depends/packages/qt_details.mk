6e82103e232aa3c97e19cf7e6d0f427b1e839dff
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-09-28 12:13:02 +0100

    depends: Factor out Qt modules' details

---
+++ depends/packages/qt_details.mk
@@ -0,0 +1,12 @@
+qt_details_version := 6.7.2
+qt_details_download_path := https://download.qt.io/official_releases/qt/6.7/$(qt_details_version)/submodules
+qt_details_suffix := everywhere-src-$(qt_details_version).tar.xz
+
+qt_details_qtbase_file_name := qtbase-$(qt_details_suffix)
+qt_details_qtbase_sha256_hash := c5f22a5e10fb162895ded7de0963328e7307611c688487b5d152c9ee64767599
+
+qt_details_qttranslations_file_name := qttranslations-$(qt_details_suffix)
+qt_details_qttranslations_sha256_hash := 9845780b5dc1b7279d57836db51aeaf2e4a1160c42be09750616f39157582ca9
+
+qt_details_qttools_file_name := qttools-$(qt_details_suffix)
+qt_details_qttools_sha256_hash := 58e855ad1b2533094726c8a425766b63a04a0eede2ed85086860e54593aa4b2a
