c77dc13a4d4a3602f3dd217b0099a2090377c97b
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-10-26 15:58:45 +0100

    cmake: Set top-level target output locations
    This change:
    1. Collects build artifacts in dedicated locations.
    2. Allows running bitcoin-chainstate.exe with bitcoinkernel.dll directly
    from the build tree on Windows.

--- contrib/devtools/gen-manpages.py
+++ contrib/devtools/gen-manpages.py
@@ -8,12 +8,12 @@ import sys
 import tempfile
 
 BINARIES = [
-'src/bitcoind',
-'src/bitcoin-cli',
-'src/bitcoin-tx',
-'src/bitcoin-wallet',
-'src/bitcoin-util',
-'src/qt/bitcoin-qt',
+'bin/bitcoind',
+'bin/bitcoin-cli',
+'bin/bitcoin-tx',
+'bin/bitcoin-wallet',
+'bin/bitcoin-util',
+'bin/bitcoin-qt',
 ]
 
 # Paths to external utilities.
