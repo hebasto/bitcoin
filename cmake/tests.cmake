4b6a842c28010a00e121fd36cc0b4e1fa658d249
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-11-06 13:22:03 +0000

    cmake, test: Disable tests instead of ignoring them

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -2,19 +2,21 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit/.
 
-if(TARGET bitcoin-util AND TARGET bitcoin-tx AND TARGET Python3::Interpreter)
-  add_test(NAME util_test_runner
-    COMMAND Python3::Interpreter ${PROJECT_BINARY_DIR}/test/util/test_runner.py
-  )
-  set_property(TEST util_test_runner PROPERTY
-    ENVIRONMENT_MODIFICATION
-      BITCOINUTIL=set:$<TARGET_FILE:bitcoin-util>
-      BITCOINTX=set:$<TARGET_FILE:bitcoin-tx>
-  )
-endif()
+add_test(NAME util_test_runner
+  COMMAND Python3::Interpreter ${PROJECT_BINARY_DIR}/test/util/test_runner.py
+)
+set_property(TEST util_test_runner PROPERTY
+  ENVIRONMENT_MODIFICATION
+    BITCOINUTIL=set:$<$<TARGET_EXISTS:bitcoin-util>:$<TARGET_FILE:bitcoin-util>>
+    BITCOINTX=set:$<$<TARGET_EXISTS:bitcoin-tx>:$<TARGET_FILE:bitcoin-tx>>
+)
+set_tests_properties(util_test_runner PROPERTIES
+  DISABLED $<NOT:$<AND:$<TARGET_EXISTS:Python3::Interpreter>,$<TARGET_EXISTS:bitcoin-util>,$<TARGET_EXISTS:bitcoin-tx>>>
+)
 
-if(TARGET Python3::Interpreter)
-  add_test(NAME util_rpcauth_test
-    COMMAND Python3::Interpreter ${PROJECT_BINARY_DIR}/test/util/rpcauth-test.py
-  )
-endif()
+add_test(NAME util_rpcauth_test
+  COMMAND Python3::Interpreter ${PROJECT_BINARY_DIR}/test/util/rpcauth-test.py
+)
+set_tests_properties(util_rpcauth_test PROPERTIES
+  DISABLED $<NOT:$<TARGET_EXISTS:Python3::Interpreter>>
+)
