6280e8d65652cdccdb3468bf9fc1fb24fefd15f2
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-01-14 11:40:57 +0000

    depends: Define `__BSD_VISIBLE` for NetBSD

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -11,7 +11,7 @@ $(package)_config_opts=--disable-shared --enable-cxx --disable-replication --ena
 $(package)_config_opts_mingw32=--enable-mingw
 $(package)_cflags+=-Wno-error=implicit-function-declaration -Wno-error=format-security -Wno-error=implicit-int
 $(package)_cppflags_freebsd=-D_XOPEN_SOURCE=600 -D__BSD_VISIBLE=1
-$(package)_cppflags_netbsd=-D_XOPEN_SOURCE=600
+$(package)_cppflags_netbsd=-D_XOPEN_SOURCE=600 -D__BSD_VISIBLE=1
 $(package)_cppflags_mingw32=-DUNICODE -D_UNICODE
 endef
 
