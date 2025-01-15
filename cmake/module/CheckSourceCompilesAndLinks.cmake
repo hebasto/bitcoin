5a6231d8029a1b8ec7b6eda7141c09fcffe1805e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-01-15 17:31:29 +0000

    cmake: Convert `check_cxx_source_compiles_with_flags` to a function

--- cmake/module/CheckSourceCompilesAndLinks.cmake
+++ cmake/module/CheckSourceCompilesAndLinks.cmake
@@ -3,16 +3,29 @@
 # file COPYING or https://opensource.org/license/mit/.
 
 include_guard(GLOBAL)
-include(CheckCXXSourceCompiles)
-include(CMakePushCheckState)
 
 # This avoids running the linker.
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-macro(check_cxx_source_compiles_with_flags flags source)
-  cmake_push_check_state(RESET)
-  set(CMAKE_REQUIRED_FLAGS ${flags})
-  list(JOIN CMAKE_REQUIRED_FLAGS " " CMAKE_REQUIRED_FLAGS)
-  check_cxx_source_compiles("${source}" ${ARGN})
-  cmake_pop_check_state()
-endmacro()
+#[=[
+Check once if C++ source code can be compiled.
+
+Options:
+
+  CXXFLAGS - A list of additional flags to pass to the compiler.
+
+For historical reasons, among the CMake `CMAKE_REQUIRED_*` variables that influence
+`check_cxx_source_compiles()`, only `CMAKE_REQUIRED_FLAGS` is a string rather than
+a list. Additionally, `target_compile_options()` also expects a list of options.
+
+The `check_cxx_source_compiles_with_flags()` function handles this case and accepts
+`CXXFLAGS` as a list, simplifying the code at the caller site.
+
+]=]
+function(check_cxx_source_compiles_with_flags source result_var)
+  cmake_parse_arguments(PARSE_ARGV 2 _ "" "" CXXFLAGS)
+  list(JOIN __CXXFLAGS " " CMAKE_REQUIRED_FLAGS)
+  include(CheckCXXSourceCompiles)
+  check_cxx_source_compiles("${source}" ${result_var})
+  set(${result_var} ${${result_var}} PARENT_SCOPE)
+endfunction()
