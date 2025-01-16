796c36a5cf8fdba5b73ea02adc7825770ebfcbb1
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-01-16 09:44:16 +0000

    ci: Disable Python in CentOS Stream 9
    The minimum required Python version is unavailable in CentOS Stream 9
    repositories.

--- ci/test/00_setup_env_i686_centos.sh
+++ ci/test/00_setup_env_i686_centos.sh
@@ -10,8 +10,9 @@ export HOST=i686-pc-linux-gnu
 export CONTAINER_NAME=ci_i686_centos
 export CI_IMAGE_NAME_TAG="quay.io/centos/amd64:stream9"
 export STREAM_GCC_V="12"
-export CI_BASE_PACKAGES="gcc-toolset-${STREAM_GCC_V}-gcc-c++ glibc-devel.x86_64 gcc-toolset-${STREAM_GCC_V}-libstdc++-devel.x86_64 glibc-devel.i686 gcc-toolset-${STREAM_GCC_V}-libstdc++-devel.i686 ccache make git python3 python3-pip which patch lbzip2 xz procps-ng dash rsync coreutils bison e2fsprogs cmake"
-export PIP_PACKAGES="pyzmq"
+export CI_BASE_PACKAGES="gcc-toolset-${STREAM_GCC_V}-gcc-c++ glibc-devel.x86_64 gcc-toolset-${STREAM_GCC_V}-libstdc++-devel.x86_64 glibc-devel.i686 gcc-toolset-${STREAM_GCC_V}-libstdc++-devel.i686 ccache make git which patch lbzip2 xz procps-ng dash rsync coreutils bison e2fsprogs cmake"
 export GOAL="install"
-export BITCOIN_CONFIG="-DWITH_ZMQ=ON -DBUILD_GUI=ON -DREDUCE_EXPORTS=ON"
+# The minimum required Python version is unavailable in CentOS Stream 9.
+export BITCOIN_CONFIG="-DWITH_ZMQ=ON -DBUILD_GUI=ON -DREDUCE_EXPORTS=ON -DWITH_PYTHON=OFF"
+export RUN_FUNCTIONAL_TESTS=false
 export CONFIG_SHELL="/bin/dash"
