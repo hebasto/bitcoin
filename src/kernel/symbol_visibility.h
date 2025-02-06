2f489a43a4ddb681f820847c3854b141d61632ec
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-02-06 15:15:27 +0000

    fixup

--- src/kernel/symbol_visibility.h
+++ src/kernel/symbol_visibility.h
@@ -1,18 +1,14 @@
 #ifndef BITCOIN_KERNEL_SYMBOL_VISIBILITY_H
 #define BITCOIN_KERNEL_SYMBOL_VISIBILITY_H
 
-#if defined(bitcoinkernel_EXPORTS)
-  #if defined(_WIN32)
+#if defined(_WIN32)
+  #if defined(bitcoinkernel_EXPORTS)
     #define BITCOINKERNEL_EXPORT_SYMBOL __declspec(dllexport)
   #else
-    #define BITCOINKERNEL_EXPORT_SYMBOL __attribute__ ((visibility ("default")))
+    #define BITCOINKERNEL_EXPORT_SYMBOL __declspec(dllimport)
   #endif
-#elif defined(_WIN32) && !defined(STATIC_LIBBITCOINKERNEL)
-  #define BITCOINKERNEL_EXPORT_SYMBOL __declspec(dllimport)
-#endif
-
-#ifndef BITCOINKERNEL_EXPORT_SYMBOL
-  #define BITCOINKERNEL_EXPORT_SYMBOL
+#else
+  #define BITCOINKERNEL_EXPORT_SYMBOL extern
 #endif
 
 #endif // BITCOIN_KERNEL_SYMBOL_VISIBILITY_H
