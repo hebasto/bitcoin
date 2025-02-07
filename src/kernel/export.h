624caba79c6408f1cfded0f1eaba5975127e88cc
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-02-07 12:24:42 +0000

    cory's version

--- src/kernel/export.h
+++ src/kernel/export.h
@@ -1,24 +1,18 @@
 #ifndef BITCOIN_KERNEL_EXPORT_H
 #define BITCOIN_KERNEL_EXPORT_H
 
-#ifndef BITCOINKERNEL_STATIC
-#  ifndef BITCOINKERNEL_EXPORT
-#    ifdef bitcoinkernel_EXPORTS
-       /* We are building this library */
-#      if defined(_WIN32)
-#        define BITCOINKERNEL_EXPORT __declspec(dllexport)
-#      endif
-#    else
-       /* We are using this library */
-#      if defined(_WIN32)
-#        define BITCOINKERNEL_EXPORT __declspec(dllimport)
-#      endif
-#    endif
-#  endif
+#if defined(bitcoinkernel_EXPORTS)
+  #if defined(_WIN32)
+    #define BITCOINKERNEL_EXPORT __declspec(dllexport)
+  #else
+    #define BITCOINKERNEL_EXPORT __attribute__ ((visibility ("default")))
+  #endif
+#elif defined(_WIN32) && !defined(BITCOINKERNEL_STATIC)
+  #define BITCOINKERNEL_EXPORT __declspec(dllimport)
 #endif
 
 #ifndef BITCOINKERNEL_EXPORT
-#  define BITCOINKERNEL_EXPORT
+  #define BITCOINKERNEL_EXPORT
 #endif
 
 #endif // BITCOIN_KERNEL_EXPORT_H
