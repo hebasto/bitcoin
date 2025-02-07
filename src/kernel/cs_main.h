e7cab8e2a9e2429f07fe6d30edaf9cea539ce251
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-02-07 10:15:46 +0000

    cmake: Use `GenerateExportHeader` module

--- src/kernel/cs_main.h
+++ src/kernel/cs_main.h
@@ -5,7 +5,7 @@
 #ifndef BITCOIN_KERNEL_CS_MAIN_H
 #define BITCOIN_KERNEL_CS_MAIN_H
 
-#include <kernel/symbol_visibility.h>
+#include <kernel/bitcoinkernel_export.h>
 #include <sync.h>
 
 /**
@@ -18,6 +18,6 @@
  * The transaction pool has a separate lock to allow reading from it and the
  * chainstate at the same time.
  */
-BITCOINKERNEL_EXPORT_SYMBOL extern RecursiveMutex cs_main;
+BITCOINKERNEL_EXPORT extern RecursiveMutex cs_main;
 
 #endif // BITCOIN_KERNEL_CS_MAIN_H
