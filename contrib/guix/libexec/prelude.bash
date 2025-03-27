c6c6c1f87af77fd6768e40b6815d796370b3f56c
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-03-27 10:23:27 +0000

    guix: Bump up to 2877c75dc5db0dbf664fb6170d5754068e941d91
    This commit updates the `libcxx` package to version 19.1.7.
    
    Required for the following switching to `libc++`.

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -51,7 +51,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://git.savannah.gnu.org/git/guix.git \
-                      --commit=53396a22afc04536ddf75d8f82ad2eafa5082725 \
+                      --commit=2877c75dc5db0dbf664fb6170d5754068e941d91 \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \
