2ce12099f38f81f4bcaff3d11684fa4ed5e83128
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-03-29 10:48:50 +0000

    depends: Switch from multilib to platform-specific toolchains

--- ci/test/00_setup_env_i686_multiprocess.sh
+++ ci/test/00_setup_env_i686_multiprocess.sh
@@ -6,11 +6,11 @@
 
 export LC_ALL=C.UTF-8
 
-export HOST=i686-pc-linux-gnu
+export HOST=i686-linux-gnu
 export CONTAINER_NAME=ci_i686_multiprocess
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
 export CI_IMAGE_PLATFORM="linux/amd64"
-export PACKAGES="llvm clang g++-multilib"
+export PACKAGES="llvm clang g++-i686-linux-gnu binutils-i686-linux-gnu"
 export DEP_OPTS="DEBUG=1 MULTIPROCESS=1"
 export GOAL="install"
 export TEST_RUNNER_EXTRA="--v2transport"
