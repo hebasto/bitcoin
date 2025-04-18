40dc8780b95d70a1fddbf9f219eaef3adca2577d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-04-18 23:22:55 +0100

    A fix?

--- src/bench/wallet_migration.cpp
+++ src/bench/wallet_migration.cpp
@@ -63,8 +63,8 @@ static void WalletMigration(benchmark::Bench& bench)
         batch.WriteKey(pubkey, key.GetPrivKey(), CKeyMetadata());
     }
 
-    bench.epochs(/*numEpochs=*/1).run([&context, &wallet] {
-        util::Result<MigrationResult> res = MigrateLegacyToDescriptor(std::move(wallet), /*passphrase=*/"", context, /*was_loaded=*/false);
+    bench.epochs(/*numEpochs=*/1).run([&context, w = std::move(wallet)] {
+        util::Result<MigrationResult> res = MigrateLegacyToDescriptor(std::move(w), /*passphrase=*/"", context, /*was_loaded=*/false);
         assert(res);
         assert(res->wallet);
         assert(res->watchonly_wallet);
