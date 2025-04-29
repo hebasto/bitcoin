60469216e3b3bf62577bd5585537999939ae7724
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-04-29 21:07:47 +0100

    Remove no longer necessary `fsbridge::fopen` class
    TODO: Consider switching to `fopen_s()`.

--- src/util/fs.cpp
+++ src/util/fs.cpp
@@ -25,12 +25,7 @@ namespace fsbridge {
 
 FILE *fopen(const fs::path& p, const char *mode)
 {
-#ifndef WIN32
     return ::fopen(p.c_str(), mode);
-#else
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>,wchar_t> utf8_cvt;
-    return ::_wfopen(p.wstring().c_str(), utf8_cvt.from_bytes(mode).c_str());
-#endif
 }
 
 fs::path AbsPathJoin(const fs::path& base, const fs::path& path)
