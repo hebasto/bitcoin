c318afef7615a74ada4f45d306963b0ecfc1df8e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-04-29 16:02:21 +0100

    Remove no longer necessary `WinCmdLineArgs` class
    This change removes one use case of `std::wstring_convert`, which is
    deprecated in C++17 and removed in C++26. Other uses remain for now.

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -1313,8 +1313,6 @@ static int CommandLineRPC(int argc, char *argv[])
 MAIN_FUNCTION
 {
 #ifdef WIN32
-    common::WinCmdLineArgs winArgs;
-    std::tie(argc, argv) = winArgs.get();
     common::WindowsScopedCodePage windows_terminal_output_code_page{CP_UTF8};
 #endif
     SetupEnvironment();
