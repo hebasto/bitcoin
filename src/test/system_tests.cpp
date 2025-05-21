e3609039e5b520f3584bf6077e2bd2d0259370d3
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-05-21 14:43:46 +0100

    Adjust `system_tests.cpp`

--- src/test/system_tests.cpp
+++ src/test/system_tests.cpp
@@ -7,12 +7,16 @@
 #include <test/util/setup_common.h>
 #include <common/run_command.h>
 #include <univalue.h>
+#include <util/string.h>
 
 #ifdef ENABLE_EXTERNAL_SIGNER
 #include <util/subprocess.h>
 #endif // ENABLE_EXTERNAL_SIGNER
 
 #include <boost/test/unit_test.hpp>
+
+#include <string>
+#include <vector>
 
 BOOST_FIXTURE_TEST_SUITE(system_tests, BasicTestingSetup)
 
@@ -21,14 +25,14 @@ BOOST_FIXTURE_TEST_SUITE(system_tests, BasicTestingSetup)
 BOOST_AUTO_TEST_CASE(run_command)
 {
     {
-        const UniValue result = RunCommandParseJSON("");
+        const UniValue result = RunCommandParseJSON({});
         BOOST_CHECK(result.isNull());
     }
     {
 #ifdef WIN32
-        const UniValue result = RunCommandParseJSON("cmd.exe /c echo {\"success\": true}");
+        const UniValue result = RunCommandParseJSON({"cmd.exe", "/c", "echo", "{\"success\": true}"});
 #else
-        const UniValue result = RunCommandParseJSON("echo {\"success\": true}");
+        const UniValue result = RunCommandParseJSON({"echo", "{\"success\": true}"});
 #endif
         BOOST_CHECK(result.isObject());
         const UniValue& success = result.find_value("success");
@@ -42,32 +46,32 @@ BOOST_AUTO_TEST_CASE(run_command)
 #else
         const std::string expected{"execve failed: "};
 #endif
-        BOOST_CHECK_EXCEPTION(RunCommandParseJSON("invalid_command"), subprocess::CalledProcessError, HasReason(expected));
+        BOOST_CHECK_EXCEPTION(RunCommandParseJSON({"invalid_command"}), subprocess::CalledProcessError, HasReason(expected));
     }
     {
         // Return non-zero exit code, no output to stderr
 #ifdef WIN32
-        const std::string command{"cmd.exe /c exit 1"};
+        const std::vector<std::string> command{"cmd.exe", "/c", "exit 1"};
 #else
-        const std::string command{"false"};
+        const std::vector<std::string> command{"false"};
 #endif
         BOOST_CHECK_EXCEPTION(RunCommandParseJSON(command), std::runtime_error, [&](const std::runtime_error& e) {
             const std::string what{e.what()};
-            BOOST_CHECK(what.find(strprintf("RunCommandParseJSON error: process(%s) returned 1: \n", command)) != std::string::npos);
+            BOOST_CHECK(what.find(strprintf("RunCommandParseJSON error: process(%s) returned 1: \n", util::Join(command, " "))) != std::string::npos);
             return true;
         });
     }
     {
         // Return non-zero exit code, with error message for stderr
 #ifdef WIN32
-        const std::string command{"cmd.exe /c \"echo err 1>&2 && exit 1\""};
+        const std::vector<std::string> command{"cmd.exe", "/c", "echo err 1>&2 && exit 1"};
 #else
-        const std::string command{"sh -c 'echo err 1>&2 && false'"};
+        const std::vector<std::string> command{"sh", "-c", "echo err 1>&2 && false"};
 #endif
         const std::string expected{"err"};
         BOOST_CHECK_EXCEPTION(RunCommandParseJSON(command), std::runtime_error, [&](const std::runtime_error& e) {
             const std::string what(e.what());
-            BOOST_CHECK(what.find(strprintf("RunCommandParseJSON error: process(%s) returned", command)) != std::string::npos);
+            BOOST_CHECK(what.find(strprintf("RunCommandParseJSON error: process(%s) returned", util::Join(command, " "))) != std::string::npos);
             BOOST_CHECK(what.find(expected) != std::string::npos);
             return true;
         });
@@ -75,16 +79,16 @@ BOOST_AUTO_TEST_CASE(run_command)
     {
         // Unable to parse JSON
 #ifdef WIN32
-        const std::string command{"cmd.exe /c echo {"};
+        const std::vector<std::string> command{"cmd.exe", "/c", "echo {"};
 #else
-        const std::string command{"echo {"};
+        const std::vector<std::string> command{"echo", "{"};
 #endif
         BOOST_CHECK_EXCEPTION(RunCommandParseJSON(command), std::runtime_error, HasReason("Unable to parse JSON: {"));
     }
 #ifndef WIN32
     {
         // Test stdin
-        const UniValue result = RunCommandParseJSON("cat", "{\"success\": true}");
+        const UniValue result = RunCommandParseJSON({"cat"}, "{\"success\": true}");
         BOOST_CHECK(result.isObject());
         const UniValue& success = result.find_value("success");
         BOOST_CHECK(!success.isNull());
