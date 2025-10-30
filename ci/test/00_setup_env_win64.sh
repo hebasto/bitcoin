bcfa40f7db80b0d2034662132fd083f585943dc9
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-10-30 13:17:48 +0000

    Drop diffutils

--- ci/test/00_setup_env_win64.sh
+++ ci/test/00_setup_env_win64.sh
@@ -8,9 +8,9 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_win64
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/fedora:42"  # Check that https://fedora.pkgs.org/42/fedora-x86_64/ucrt64-gcc-c++-14.2.1-4.fc42.x86_64.rpm.html can cross-compile
-export CI_BASE_PACKAGES="cmake diffutils gawk make patch procps-ng rsync ucrt64-gcc-c++ util-linux which"
+export CI_BASE_PACKAGES="cmake gawk make patch procps-ng rsync ucrt64-gcc-c++ util-linux which"
 export HOST=x86_64-w64-mingw32ucrt
-export DEP_OPTS="CC=/usr/bin/${HOST}-gcc mingw32_CXX=/usr/bin/${HOST}-g++ NO_QT=1"
+export DEP_OPTS="CC=/usr/bin/${HOST}-gcc CXX=/usr/bin/${HOST}-g++ NO_QT=1"
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
