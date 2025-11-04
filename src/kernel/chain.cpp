9a87311c8832287ae424367ed3d11c58b5b87cce
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-11-04 16:59:54 +0000

    ci, iwyu: Fix warnings in `src/kernel` and treat them as errors

--- src/kernel/chain.cpp
+++ src/kernel/chain.cpp
@@ -2,11 +2,14 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <kernel/chain.h>
+
 #include <chain.h>
 #include <interfaces/chain.h>
-#include <kernel/chain.h>
 #include <sync.h>
 #include <uint256.h>
+
+#include <kernel/cs_main.h>
 
 class CBlock;
 
