a9650999dd9637a7991cd4511f4437cbad7eda34
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-11-19 22:42:05 +0000

    depends: libxcb 1.17.0

--- depends/packages/libxcb.mk
+++ depends/packages/libxcb.mk
@@ -1,8 +1,8 @@
 package=libxcb
-$(package)_version=1.15
+$(package)_version=1.17.0
 $(package)_download_path=https://xcb.freedesktop.org/dist
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=1cb65df8543a69ec0555ac696123ee386321dfac1964a3da39976c9a05ad724d
+$(package)_sha256_hash=2c69287424c9e2128cb47ffe92171e10417041ec2963bceafb65cb3fcf8f0b85
 $(package)_dependencies=xcb_proto libXau
 $(package)_patches = remove_pthread_stubs.patch
 
