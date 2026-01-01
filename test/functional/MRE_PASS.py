4667bcbfcd00fa986ed035f211ff8d9a87d4f38f
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-01-01 11:24:06 +0000

    qa: Add `MRE_*.py`

---
+++ test/functional/MRE_PASS.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# File: test/functional/MRE.py
+
+from test_framework.test_framework import BitcoinTestFramework
+
+class MRE(BitcoinTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.setup_clean_chain = True
+
+    def skip_test_if_missing_module(self):
+        self.skip_if_no_wallet()
+
+    def run_test(self):
+        self.log.info("OK")
+
+if __name__ == '__main__':
+    MRE(__file__).main()
