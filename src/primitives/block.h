3a3f99e06a795e70ddc77b2ea26ea494dc50bc41
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-01-02 15:32:05 +0000

    reduce 06

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -1,59 +1,17 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-present The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 #ifndef BITCOIN_PRIMITIVES_BLOCK_H
 #define BITCOIN_PRIMITIVES_BLOCK_H
 
-#include <primitives/transaction.h>
 #include <serialize.h>
-#include <uint256.h>
-#include <util/time.h>
 
-/** Nodes collect new transactions into a block, hash them into a hash tree,
- * and scan through nonce values to make the block's hash satisfy proof-of-work
- * requirements.  When they solve the proof-of-work, they broadcast the block
- * to everyone and the block is added to the block chain.  The first transaction
- * in the block is a special one that creates a new coin owned by the creator
- * of the block.
- */
-class CBlockHeader
+struct CBlockHeader
 {
-public:
-    // header
-    int32_t nVersion;
-    uint256 hashPrevBlock;
-    uint256 hashMerkleRoot;
-    uint32_t nTime;
-    uint32_t nBits;
-    uint32_t nNonce;
-
-    CBlockHeader()
-    {
-        SetNull();
-    }
+    int32_t n;
 
     template <typename Stream>
     void Unserialize(Stream& s)
     {
         auto ser_action = ActionUnserialize{};
-        ser_action.SerReadWriteMany(s, nVersion, hashPrevBlock, hashMerkleRoot, nTime, nBits, nNonce);
-    }
-
-    void SetNull()
-    {
-        nVersion = 0;
-        hashPrevBlock.SetNull();
-        hashMerkleRoot.SetNull();
-        nTime = 0;
-        nBits = 0;
-        nNonce = 0;
-    }
-
-    bool IsNull() const
-    {
-        return (nBits == 0);
+        ser_action.SerReadWriteMany(s, n);
     }
 };
 
