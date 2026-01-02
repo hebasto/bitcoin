be53eefc32941bf969c6901363e3927e75e279e0
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-01-02 14:35:49 +0000

    reduce 02

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -8,23 +8,3 @@
 #include <hash.h>
 #include <tinyformat.h>
 
-uint256 CBlockHeader::GetHash() const
-{
-    return (HashWriter{} << *this).GetHash();
-}
-
-std::string CBlock::ToString() const
-{
-    std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
-        GetHash().ToString(),
-        nVersion,
-        hashPrevBlock.ToString(),
-        hashMerkleRoot.ToString(),
-        nTime, nBits, nNonce,
-        vtx.size());
-    for (const auto& tx : vtx) {
-        s << "  " << tx->ToString() << "\n";
-    }
-    return s.str();
-}
