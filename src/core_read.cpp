adf46f5361e9a5c52a99f6b3211ad8910946b133
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-01-02 14:26:49 +0000

    iwyu: Trigger warning

--- src/core_read.cpp
+++ src/core_read.cpp
@@ -4,7 +4,7 @@ The full include-list for /ci_container_base/src/core_read.cpp:
 #include <streams.h>           // for DataStream
 */
 
-#include <primitives/block.h> // IWYU pragma: keep
+#include <primitives/block.h>
 
 template <class T, class Stream>
 concept Unserializable = requires(T a, Stream s) { a.Unserialize(s); };
