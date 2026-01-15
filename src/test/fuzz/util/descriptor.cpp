2ea0d0868f7949ee336589f30306d0505fb5bba1
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-01-15 15:47:22 +0000

    step 06

--- src/test/fuzz/util/descriptor.cpp
+++ src/test/fuzz/util/descriptor.cpp
@@ -6,6 +6,8 @@
 
 std::array<std::byte, 32> key_data{std::byte{1}};
 
+static constexpr uint8_t KEY_TYPES_COUNT{6};
+
 static bool IdIsUnCompPubKey(uint8_t idx) { return idx % KEY_TYPES_COUNT == 1; }
 
 static void foo()
