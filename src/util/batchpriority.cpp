68d9a26baf29bc535a577e1ed6e8ba69c4274fdb
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-01-30 15:10:54 +0000

    util: Drop *BSD headers in `batchpriority.cpp`
    Currently, there are issues with headers in `batchpriority.cpp`:
    1. `SCHED_BATCH` is not defined on all supported *BSD platforms.
    2. `pthread.h` is necessary on other platforms.
    
    This addresses both issues and fixes other includes.

--- src/util/batchpriority.cpp
+++ src/util/batchpriority.cpp
@@ -2,14 +2,14 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <util/batchpriority.h>
+
 #include <logging.h>
 #include <util/syserror.h>
 
-#if (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__))
-#include <pthread.h>
-#include <pthread_np.h>
-#endif
+#include <string>
 
+#include <pthread.h>
 #ifndef WIN32
 #include <sched.h>
 #endif
