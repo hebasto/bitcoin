e6b09124658edaf14f66a25f86acf039b440731c
fanquake <fanquake@gmail.com>
2026-02-03 11:58:43 +0000

    ci: add openBSD cross CI job

---
+++ ci/test/00_setup_env_openbsd_cross.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit.
+
+export LC_ALL=C.UTF-8
+
+export CONTAINER_NAME=ci_openbsd_cross
+export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
+export APT_LLVM_V="22"
+export OPENBSD_VERSION=7.9
+export PACKAGES="clang-${APT_LLVM_V} llvm-${APT_LLVM_V} lld"
+export HOST=x86_64-unknown-openbsd
+export DEP_OPTS="build_CC=clang build_CXX=clang++ LDFLAGS='-fuse-ld=lld' AR=llvm-ar-${APT_LLVM_V} STRIP=llvm-strip-${APT_LLVM_V} NM=llvm-nm-${APT_LLVM_V} RANLIB=llvm-ranlib-${APT_LLVM_V}"
+export GOAL="install"
+export BITCOIN_CONFIG="\
+ --preset=dev-mode \
+ -DBUILD_KERNEL_LIB=OFF \
+ -DBUILD_UTIL_CHAINSTATE=OFF \
+ -DREDUCE_EXPORTS=ON \
+ -DWITH_USDT=OFF \
+"
+export RUN_UNIT_TESTS="false"
+export RUN_FUNCTIONAL_TESTS="false"
