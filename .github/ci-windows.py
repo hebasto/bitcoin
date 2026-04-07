e166b9f2f06e8edc972f94521c30d8f56c7a212f
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-04-07 21:42:29 +0100

    Fix build type

--- .github/ci-windows.py
+++ .github/ci-windows.py
@@ -68,6 +68,11 @@ def github_import_vs_env(_ci_type):
 def generate(ci_type):
     command = [
         "cmake",
+        "-E",
+        "env",
+        # See https://github.com/microsoft/vcpkg/issues/50927.
+        "VCPKG_DEFAULT_HOST_TRIPLET=x64-windows",
+        "cmake",
         "-B",
         "build",
         "-Werror=dev",
