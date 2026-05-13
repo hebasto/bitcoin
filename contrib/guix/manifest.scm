hebasto/bitcoin

commit 2276426bb1b9c028d842bf26dcf8004613e9eae9

guix: switch to upstream python-oscrypto package

contrib/guix/manifest.scm
@@ -16,7 +16,7 @@
              (gnu packages pkg-config)
              ((gnu packages python) #:select (python-minimal))
              ((gnu packages python-build) #:select (python-poetry-core))
-             ((gnu packages python-crypto) #:select (python-asn1crypto))
+             ((gnu packages python-crypto) #:select (python-asn1crypto python-oscrypto))
              ((gnu packages python-xyz) #:select (python-lief))
              ((gnu packages tls) #:select (openssl))
              ((gnu packages version-control) #:select (git-minimal))
@@ -176,64 +176,6 @@ chain for " target " development."))
       (description "elfesteem parses ELF, PE and Mach-O files.")
       (license license:lgpl2.1))))
 
-(define-public python-oscrypto
-  (package
-    (name "python-oscrypto")
-    (version "1.3.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/wbond/oscrypto")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32
-         "1v5wkmzcyiqy39db8j2dvkdrv2nlsc48556h73x4dzjwd6kg4q0a"))
-       (patches (search-our-patches "oscrypto-hard-code-openssl.patch"))))
-    (build-system python-build-system)
-    (native-search-paths
-     (list (search-path-specification
-            (variable "SSL_CERT_FILE")
-            (file-type 'regular)
-            (separator #f)                ;single entry
-            (files '("etc/ssl/certs/ca-certificates.crt")))))
-
-    (propagated-inputs
-      (list python-asn1crypto openssl))
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'hard-code-path-to-libscrypt
-           (lambda* (#:key inputs #:allow-other-keys)
-             (let ((openssl (assoc-ref inputs "openssl")))
-               (substitute* "oscrypto/__init__.py"
-                 (("@GUIX_OSCRYPTO_USE_OPENSSL@")
-                  (string-append openssl "/lib/libcrypto.so" "," openssl "/lib/libssl.so")))
-               #t)))
-         (add-after 'unpack 'disable-broken-tests
-           (lambda _
-             ;; This test is broken as there is no keyboard interrupt.
-             (substitute* "tests/test_trust_list.py"
-               (("^(.*)class TrustListTests" line indent)
-                (string-append indent
-                               "@unittest.skip(\"Disabled by Guix\")\n"
-                               line)))
-             (substitute* "tests/test_tls.py"
-               (("^(.*)class TLSTests" line indent)
-                (string-append indent
-                               "@unittest.skip(\"Disabled by Guix\")\n"
-                               line)))
-             #t))
-         (replace 'check
-           (lambda _
-             (invoke "python" "run.py" "tests")
-             #t)))))
-    (home-page "https://github.com/wbond/oscrypto")
-    (synopsis "Compiler-free Python crypto library backed by the OS")
-    (description "oscrypto is a compilation-free, always up-to-date encryption library for Python.")
-    (license license:expat)))
-
 (define-public python-oscryptotests
   (package (inherit python-oscrypto)
     (name "python-oscryptotests")
@@ -265,7 +207,8 @@ chain for " target " development."))
            "1qw2k7xis53179lpqdqyylbcmp76lj7sagp883wmxg5i7chhc96k"))))
       (build-system python-build-system)
       (propagated-inputs
-        (list python-asn1crypto
+        (list openssl
+              python-asn1crypto
               python-oscrypto
               python-oscryptotests)) ;; certvalidator tests import oscryptotests
       (arguments

contrib/guix/patches/oscrypto-hard-code-openssl.patch
@@ -1,13 +1,0 @@
-diff --git a/oscrypto/__init__.py b/oscrypto/__init__.py
-index eb27313..371ab24 100644
---- a/oscrypto/__init__.py
-+++ b/oscrypto/__init__.py
-@@ -302,3 +302,8 @@ def load_order():
-         'oscrypto._win.tls',
-         'oscrypto.tls',
-     ]
-+
-+
-+paths = '@GUIX_OSCRYPTO_USE_OPENSSL@'.split(',')
-+assert len(paths) == 2, 'Value for OSCRYPTO_USE_OPENSSL env var must be two paths separated by a comma'
-+use_openssl(*paths)

commit 04a06cbcf2bdb7830678e56c8d790672900ff17d

guix: Hard-code paths to OpenSSL's libraries
Due to Python's `ctypes.util.find_library()` implementation details,
the `python-signapple` package implicitly depends on `clang-toolchain`.

This regression was inadvertently introduced in commit
2276426bb1b9c028d842bf26dcf8004613e9eae9.

Fixing this regression is a prerequisite for the subsequent commit.

contrib/guix/manifest.scm
@@ -291,7 +291,8 @@ specific moment in time, whitelisting and revocation checks.")
          (file-name (git-file-name name commit))
          (sha256
           (base32
-           "17yqjll8nw83q6dhgqhkl7w502z5vy9sln8m6mlx0f1c10isg8yg"))))
+           "17yqjll8nw83q6dhgqhkl7w502z5vy9sln8m6mlx0f1c10isg8yg"))
+         (patches (search-our-patches "signapple-hard-code-openssl.patch"))))
       (build-system pyproject-build-system)
       (propagated-inputs
         (list python-asn1crypto
@@ -299,9 +300,19 @@ specific moment in time, whitelisting and revocation checks.")
               python-certvalidator
               python-elfesteem))
       (native-inputs (list python-poetry-core))
-      ;; There are no tests, but attempting to run python setup.py test leads to
-      ;; problems, just disable the test
-      (arguments '(#:tests? #f))
+      (arguments
+       ;; There are no tests, but attempting to run python setup.py test leads to
+       ;; problems, just disable the test
+       `(#:tests? #f
+         #:phases
+         (modify-phases %standard-phases
+           (add-after 'unpack 'hard-code-path-to-openssl
+             (lambda* (#:key inputs #:allow-other-keys)
+               (let ((openssl (assoc-ref inputs "openssl")))
+                 (substitute* "signapple/__init__.py"
+                   (("@GUIX_OSCRYPTO_USE_OPENSSL@")
+                    (string-append openssl "/lib/libcrypto.so" "," openssl "/lib/libssl.so")))
+                 #t))))))
       (home-page "https://github.com/achow101/signapple")
       (synopsis "Mach-O binary signature tool")
       (description "signapple is a Python tool for creating, verifying, and

contrib/guix/patches/signapple-hard-code-openssl.patch
@@ -1,0 +1,19 @@
+The `oscrypto` library searches for the OpenSSL runtime dependency using
+`ctypes.util.find_library()`, which implicitly requires a toolchain.
+See:
+ - https://github.com/wbond/oscrypto/blob/1.3.0/readme.md#dependencies
+ - https://docs.python.org/3/library/ctypes.html#ctypes.util.find_library
+ - https://docs.python.org/3/library/ctypes.html#ctypes-finding-shared-libraries
+
+This patch hard-codes the paths to `libcrypto.so` and `libssl.so`, making
+the `signapple` package self-consistent.
+
+--- a/signapple/__init__.py
++++ b/signapple/__init__.py
+@@ -1,3 +1,6 @@
++import oscrypto
++oscrypto.use_openssl(*'@GUIX_OSCRYPTO_USE_OPENSSL@'.split(','))
++
+ import argparse
+ 
+ 