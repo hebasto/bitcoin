dfb3281ffb72978a4b6fa3fd029fa411298dba15
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-05-31 15:18:29 +0100

    depends: Patch `libxcb_util_keysyms`

--- depends/packages/libxcb_util_keysyms.mk
+++ depends/packages/libxcb_util_keysyms.mk
@@ -4,6 +4,7 @@ $(package)_download_path=https://xcb.freedesktop.org/dist
 $(package)_file_name=xcb-util-keysyms-$($(package)_version).tar.gz
 $(package)_sha256_hash=1fa21c0cea3060caee7612b6577c1730da470b88cbdf846fa4e3e0ff78948e54
 $(package)_dependencies=libxcb xproto
+$(package)_patches = openbsd_ranlib.patch
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --disable-devel-docs --without-doxygen
@@ -11,7 +12,8 @@ $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
 endef
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . && \
+  patch -p1 -i $($(package)_patch_dir)/openbsd_ranlib.patch
 endef
 
 define $(package)_config_cmds
