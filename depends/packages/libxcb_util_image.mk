01eae97ab6dfb6f8c8aab9586be485151d74dce4
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-05-31 15:10:26 +0100

    depends: Patch `libxcb_util_image`

--- depends/packages/libxcb_util_image.mk
+++ depends/packages/libxcb_util_image.mk
@@ -4,6 +4,7 @@ $(package)_download_path=https://xcb.freedesktop.org/dist
 $(package)_file_name=xcb-util-image-$($(package)_version).tar.gz
 $(package)_sha256_hash=0ebd4cf809043fdeb4f980d58cdcf2b527035018924f8c14da76d1c81001293b
 $(package)_dependencies=libxcb libxcb_util
+$(package)_patches = openbsd_ranlib.patch
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --disable-devel-docs --without-doxygen
@@ -11,7 +12,8 @@ $(package)_config_opts+= --disable-dependency-tracking --enable-option-checking
 endef
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . && \
+  patch -p1 -i $($(package)_patch_dir)/openbsd_ranlib.patch
 endef
 
 define $(package)_config_cmds
