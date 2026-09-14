6ed47eb3bbe6f9831e68231a26166719a366a2ad
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-09-14 15:23:46 +0100

    guix: Update time-machine to `7c6b3305a872266b9d49e30cbc9df48f0b5fb374`
    This is necessary to unblock building on RISC-V systems.
    
    Fixes https://github.com/bitcoin/bitcoin/issues/36232.

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -71,7 +71,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://codeberg.org/guix/guix.git \
-                      --commit=c5eee3336cc1d10a3cc1c97fde2809c3451624d3 \
+                      --commit=7c6b3305a872266b9d49e30cbc9df48f0b5fb374 \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \
