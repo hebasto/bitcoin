8dc601d69680beda772b1e7b7b0829664d7d1561
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-09-23 11:01:59 +0100

    Revert "ci, iwyu: generate embedded ASMap header explicitly"
    This reverts commit 1cbbf64ea81085109bd9cc594a0e1cf1cf943596.

--- src/init.cpp
+++ src/init.cpp
@@ -69,6 +69,7 @@
 #include <policy/fees/block_policy_estimator.h>
 #include <policy/fees/estimator_args.h>
 #include <policy/fees/estimator_man.h>
+#include <policy/fees/mempool_estimator.h>
 #include <policy/policy.h>
 #include <policy/settings.h>
 #include <protocol.h>
@@ -87,7 +88,6 @@
 #include <util/asmap.h>
 #include <util/batchpriority.h>
 #include <util/btcsignals.h>
-#include <util/byte_units.h>
 #include <util/chaintype.h>
 #include <util/check.h>
 #include <util/fs.h>
@@ -115,6 +115,7 @@
 #include <exception>
 #include <fstream>
 #include <functional>
+#include <initializer_list>
 #include <limits>
 #include <list>
 #include <memory>
@@ -127,6 +128,7 @@
 #include <thread>
 #include <tuple>
 #include <utility>
+#include <variant>
 #include <vector>
 
 #ifndef WIN32
--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -11,7 +11,6 @@
 #include <chainparams.h>
 #include <clientversion.h>
 #include <core_io.h>
-#include <crypto/hex_base.h>
 #include <net.h>
 #include <net_permissions.h>
 #include <net_processing.h>
@@ -52,10 +51,10 @@
 #include <atomic>
 #include <compare>
 #include <cstdint>
+#include <functional>
 #include <map>
 #include <memory>
 #include <optional>
-#include <span>
 #include <sstream>
 #include <stdexcept>
 #include <string>
